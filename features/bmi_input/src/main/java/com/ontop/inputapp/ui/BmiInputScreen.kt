be17608d5411988d2055a1583cecd9c9954db0a8
Aslm <aslmmon993@gmail.com>
2024-03-28 18:10:45 -0300

    added view for height

--- features/bmi_input/src/main/java/com/ontop/inputapp/ui/BmiInputScreen.kt
+++ features/bmi_input/src/main/java/com/ontop/inputapp/ui/BmiInputScreen.kt
@@ -76,6 +76,7 @@ fun BmiInputScreen(
                     listOf(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 23, 45, 12, 12, 12, 14, 13, 14)
                 val state = rememberLazyListState()
 
+
                 LazyRow(
                     modifier = modifier.fillMaxWidth(),
                     state = state,
