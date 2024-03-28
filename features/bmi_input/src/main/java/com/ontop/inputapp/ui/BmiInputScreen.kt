5603dba0dfdeb87b71a720c12cb63398d45d737a
Aslm <aslmmon993@gmail.com>
2024-03-28 20:01:11 -0300

    update view for Weight

--- features/bmi_input/src/main/java/com/ontop/inputapp/ui/BmiInputScreen.kt
+++ features/bmi_input/src/main/java/com/ontop/inputapp/ui/BmiInputScreen.kt
@@ -3,10 +3,12 @@ package com.ontop.inputapp.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyListLayoutInfo
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -90,12 +92,13 @@ fun BmiInputScreen(
                 }
 
             }
+            Spacer(modifier = modifier.width(50.dp))
             ContentWithTitle(title = stringResource(R.string.weight), modifier = modifier.padding(horizontal = 10.dp)) {
                 RoundedCardView(modifier = modifier) {
                     var lists = listOf(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 23, 45, 12, 12, 12, 14, 13, 14)
 
                     ScrollableRowList(modifier = modifier.size(150.dp,60.dp), content = { isSameIndex ,item->
-                        Text(modifier=modifier.padding(20.dp),text = item.toString(),
+                        Text(modifier=modifier.padding(horizontal = 20.dp, vertical = 20.dp),text = item.toString(),
                             fontWeight = if (isSameIndex) FontWeight.Bold else FontWeight.Light
                         )
                     }, lists = lists)
