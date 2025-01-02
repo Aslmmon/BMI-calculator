349896255b52351c7f0184bb68c1fbdc8088587e
Aslm <aslmmon993@gmail.com>
2025-01-02 18:32:56 -0300

    - added inches and Pounds to the calculation

--- features/bmi/src/main/java/com/ontop/inputapp/ui/result/BMIResult.kt
+++ features/bmi/src/main/java/com/ontop/inputapp/ui/result/BMIResult.kt
@@ -1,6 +1,11 @@
 package com.ontop.inputapp.ui.result
 
 import android.util.Log
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -16,6 +21,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,6 +41,7 @@ import com.ontop.inputapp.shared.ButtonWithHyperLinkContent
 import com.ontop.inputapp.shared.SharedViewModel
 import com.ontop.inputapp.shared._Gap
 import com.ontop.inputapp.ui.input.UserInputSelection
+import kotlinx.coroutines.delay
 
 @Composable
 fun BmiResultScreen(
@@ -46,13 +55,17 @@ fun BmiResultScreen(
     val bmiStatus by bmiResultViewModel.bmiStatus.collectAsState()
 
 
-
     val context = LocalContext.current
 
 
     LaunchedEffect(Unit) {
         bmiResultViewModel.calculateBMi(data.value!!, context)
+        delay(300)
+
     }
+
+
+
 
     Column(
         modifier = Modifier
